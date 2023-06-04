f0a3d9a768c544a3ab041f91cd91e5e894125bc1
Keelan-Blane <alleacco01@gmail.com>
2023-06-04 17:05:54 +0200

    4th: both html

--- Audiobase/views.py
+++ Audiobase/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from .models import Artist, Album
 
@@ -22,12 +23,14 @@ def artist_to_dict(artist):
 def album_to_dict(album):
     album_dict = {
         'idb': album.idb,
-        'ida': album.ida.ida,
+        'ida': album.ida,
         'bname': album.bname,
         'year': album.year,
         'genre': album.genre,
         'gold': album.gold,
         'plat': album.plat,
+
+        # 'songs': album.song_set.all()
     }
     return album_dict
 
@@ -39,11 +42,35 @@ def index(request):
     return render(request, 'Audiobase/index.html', {'artists': artists, 'albums': albums})
 
 
-def artist_bio(request, artist_id):
+"""def artist_bio(request, artist_id):
+
+
     context = artist_to_dict(Artist.objects.get(ida=artist_id))
+    return render(request, 'Audiobase/artist.html', context)"""
+
+def artist_bio(request, artist_id):
+    try:
+        artist_obj = Artist.objects.get(ida=artist_id)
+    except Artist.DoesNotExist:
+        raise Http404("L'artista non esiste")
+
+    albums = Album.objects.filter(ida=artist_obj)
+
+    context = {
+        'artist': artist_to_dict(artist_obj),
+        'albums': artist_obj.album_set.all()
+    }
     return render(request, 'Audiobase/artist.html', context)
 
 
 def album_bio(request, album_id):
-    context = album_to_dict(Album.objects.get(idb=album_id))
+
+    #   Check if album_id's album actually exists
+    try:
+        album_obj = Album.objects.get(idb=album_id)
+    except Album.DoesNotExist:
+        raise Http404("L'album non esiste")
+
+    context = {'album': album_to_dict(album_obj),
+               'songs': album_obj.song_set.all()}
     return render(request, 'Audiobase/album.html', context)
